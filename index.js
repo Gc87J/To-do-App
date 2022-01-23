946cb10208ba7532ad287500ad5742e1fdf45ec7
Gareth Chambers <Gareth15chambers@gmail.com>
2022-01-23 14:18:47 -0500

    A little functionality was added to the add button

---
+++ index.js
@@ -0,0 +1,34 @@
+const input1 = document.querySelector(".input-1");
+
+const addBtn = document.querySelector(".add-btn");
+
+const items = document.querySelector(".items");
+
+addBtn.addEventListener("click", add);
+//addBtn.addEventListener("click", clear);
+
+
+function add() {
+
+ 
+  add2();
+ 
+}
+
+
+
+function add2() {
+ 
+
+  
+}
+
+
+
+
+
+
+function clear() {
+
+
+}
