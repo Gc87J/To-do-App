9bb97a6b1b19b42bfb953b7817eac7b16beee895
Gc87J <91073870+Gc87J@users.noreply.github.com>
2022-02-16 11:46:39 -0500

    Finally finished my first project.

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const addTo = document.querySelector(".addto");
 
 
 const newItem = document.createElement("div")
+newItem.setAttribute("class", "newItem");
 
+const secDiv = document.createElement("div");
+
+secDiv.setAttribute("class", "secDiv")
+
+newItem.appendChild(secDiv);
 
 addTo.appendChild(newItem);
 
@@ -12,20 +18,39 @@ const addBtn = document.querySelector(".to-btn");
 
 const input = document.querySelector(".input")
 
-addBtn.addEventListener("click", testing);
-
-function testing() {
-
-
+addBtn.addEventListener("click", ()=> {
+  
+  if(input.value==="") {
+    return alert("Please enter a item")
+  }
   const para = document.createElement("p");
+
+  const inlineBlock = document.createElement("span");
+
   para.setAttribute("class", "red");
   
   para.textContent = input.value;
   
-  newItem.appendChild(para);
+  secDiv.appendChild(para);
+
+  inlineBlock.setAttribute("class", "blue")
+
+  secDiv.appendChild(inlineBlock);
   
   
   input.value = "";
 
+  const testButton = document.createElement("button");
 
-}
+  testButton.setAttribute("class", "testingBtn");
+  
+
+  para.appendChild(testButton)
+
+  testButton.addEventListener("click", ()=> {
+    para.parentNode.removeChild(para);
+  })
+
+
+});
+
